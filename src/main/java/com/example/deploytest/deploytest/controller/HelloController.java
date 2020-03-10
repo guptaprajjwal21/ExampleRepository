e63dfa5b6f4270bf9b7cb3e97171f3aca29591bc
Gupta, Prajjwal <gupta.prajjwal@principal.com>
2020-03-10 12:39:39 +0530

    TestCommit

---
+++ src/main/java/com/example/deploytest/deploytest/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.deploytest.deploytest.controller;
+
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class HelloController {
+
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String sayHello(){
+        return "Hello World Developer";
+    }
+
+}
